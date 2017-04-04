ef53f7a4e92bde8c9d49b59d8d11e4656efd301b
offain <fabian.leutenegger@bluewin.ch>
2017-04-05 00:32:18 +0200

    vdev: remove -Werror
    TZhis caused a build error on Xiaomi Mi5

--- msm8996/mm-video-v4l2/vidc/vdec/Android.mk
+++ msm8996/mm-video-v4l2/vidc/vdec/Android.mk
@@ -108,7 +108,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
-LOCAL_CFLAGS                    := $(libmm-vdec-def) -Werror
+LOCAL_CFLAGS                    := $(libmm-vdec-def)
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
 
